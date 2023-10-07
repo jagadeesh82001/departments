43bfe01a0c86a95b4228efea2b02a4f52a5830d3
kishore7468 <147248983+kishore7468@users.noreply.github.com>
2023-10-07 22:04:17 +0530

    Add files via upload

---
+++ students123/src/main/java/com/example/students123/Students123Application.java
@@ -0,0 +1,13 @@
+package com.example.students123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Students123Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Students123Application.class, args);
+	}
+
+}
---
+++ students123/src/test/java/com/example/students123/Students123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.students123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Students123ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
